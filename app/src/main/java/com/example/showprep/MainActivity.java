0e7f9a6c50f0d434b38db82eef8b489d6314b2f3
Xavier Escobar <xavier_18_96@hotmail.com>
2019-03-31 17:01:12 -0400

    Spotify integration

--- app/src/main/java/com/example/showprep/MainActivity.java
+++ app/src/main/java/com/example/showprep/MainActivity.java
@@ -1,13 +1,213 @@
 package com.example.showprep;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "07450964b36e46d6ab07178b94916e4a";
+    private static final String REDIRECT_URI = "showprep://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+    private static final int REQUEST_CODE = 1337;
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+    private String mAccessToken;
+    private String mAccessCode;
+    private Call mCall;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming", "app-remote-control"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+
+//        ConnectionParams connectionParams =
+////                new ConnectionParams.Builder(CLIENT_ID)
+////                        .setRedirectUri(REDIRECT_URI)
+////                        .showAuthView(true)
+////                        .build();
+////         connect(this, connectionParams,
+////                new Connector.ConnectionListener() {
+////
+////                    @Override
+////                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+////                        mSpotifyAppRemote = spotifyAppRemote;
+////                        Log.d("MainActivity", "Connected! Yay!");
+////
+////                        // Connection successful. Interact with App remote.
+////                        connected();
+////                    }
+////
+////                    @Override
+////                    public void onFailure(Throwable throwable) {
+////                        Log.e("MainActivity", throwable.getMessage(), throwable);
+////
+////                        // Something went wrong when attempting to connect! Handle errors here
+////                    }
+////                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        Log.d("Debug", "Back");
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    mAccessToken = response.getAccessToken();
+                    connected();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    response.getError();
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+    private void connected() {
+        Log.d("main", "connected");
+
+//        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+//        mSpotifyAppRemote.getPlayerApi()
+//                .subscribeToPlayerState()
+//                .setEventCallback(new Subscription.EventCallback<PlayerState>() {
+//                    @Override
+//                    public void onEvent(PlayerState playerState) {
+//                        final Track track = playerState.track;
+//                        if (track != null) {
+//                            Log.d("MainActivity", track.name + " by " + track.artist.name);
+//                        }
+//                    }
+//                });
+    }
+
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+//    }
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        if (uri != null) {
+            AuthenticationResponse response = AuthenticationResponse.fromUri(uri);
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    String token = response.getAccessToken();
+                    connected();
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+    public void onGetUserProfileClicked(View view) {
+        if (mAccessToken == null) {
+            Log.d("Main", "NULL ACCESS");
+            return;
+        }
+
+        final Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me")
+                .addHeader("Authorization","Bearer " + mAccessToken)
+                .build();
+
+        cancelCall();
+        mCall = mOkHttpClient.newCall(request);
+
+        mCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                setResponse("Failed to fetch data: " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    final JSONObject jsonObject = new JSONObject(response.body().string());
+                    setResponse(jsonObject.toString(3));
+                } catch (JSONException e) {
+                    setResponse("Failed to parse data: " + e);
+                }
+            }
+        });
+    }
+
+    private void setResponse(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final TextView responseView = (TextView) findViewById(R.id.textView);
+                responseView.setText(text);
+            }
+        });
+    }
+
+    private void cancelCall() {
+        if (mCall != null) {
+            mCall.cancel();
+        }
     }
 }
